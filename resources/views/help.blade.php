8278dbdb53d4665ad50de63042adabb59351e959
duarteCost <2084813@max.student.pt>
2016-12-27 13:23:10 +0000

    Parte dos tickets done

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -3,16 +3,6 @@
     <title>FC ajuda</title>
 @stop
 @section('content')
-    <form method="post"  action="/help/addTicket" >
-        <h1>ppppp</h1>
-        <div   id="add_Product" class="form-group" >
-            <fieldset class = "userState">
-                <input class = "userState" type="hidden" name="_token" value="{{csrf_token()}}">
-                <input class = "userState" type="submit" name="submitTicket" value="Adicionar Ticket">
-            </fieldset>
-            <br>
-        </div>
-    </form>
 @stop
 @section('footer')
 @stop
